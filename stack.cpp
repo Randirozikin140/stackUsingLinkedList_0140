409cf682efd21488a758efbb02e8b4f0417db3a3
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-06-05 14:54:12 +0700

    implementasikan methode push dalam class stack  untuk menambahkan node baru

--- stack.cpp
+++ stack.cpp
@@ -22,4 +22,13 @@ public:
     {
         top = NULL;
     }
+
+    int push(int value)
+    {
+        Node *newNode = new Node(); //1. Allocate memory for new node
+        newNode->data = value;      //2. Assign value
+        newNode->next = top;        //3. set the next pointer of the new node to the current top
+        top = newNode;              //4. Update the top pointer to the new node   
+    }
 };
+
